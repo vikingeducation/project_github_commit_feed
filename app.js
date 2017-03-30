11b3fd27ac683310f34e8aeb482f3035b064ea6c
William Whitworth <william10charles@gmail.com>
2017-03-30 11:31:44 -0600

    used wrapper to dispaly commits

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const http = require("http");
 const fs = require("fs");
 const commits = require("./data/commits");
-const url = require('url');
+const url = require("url");
 var github_wrapper = require("./github_wrapper");
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  let info_obj = parseUrl(req.url);
+
   res.setHeader("Content-Type", "text/html");
   fs.readFile("./public/index.html", (err, data) => {
+    let regex = /{{commitFeed}}/;
+    let info_obj = parseUrl(req.url);
+
     if (err) {
       throw err;
+    } else if (!info_obj) {
+      res.end(
+        data.toString().replace(regex, "This is where the commits will live.")
+      );
+    } else {
+      github_wrapper.getCommits(info_obj, function(userCommits) {
+        let scrubbedData = scrubTheData(userCommits);
+        res.end(
+          data.toString().replace(regex, JSON.stringify(userCommits, null, 2))
+        );
+      });
     }
-
-    github_wrapper.getCommits(info_obj, function(userCommits) {
-      let regex = /{{commitFeed}}/;
-      res.end(data.toString().replace(regex, JSON.stringify(userCommits, null, 2)));
-    })
   });
 });
 
@@ -26,13 +35,15 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-
 function parseUrl(urlData) {
-  let regex = /user=(.+)&repo=(.+)/
- 
+  let regex = /user=(.+)&repo=(.+)/;
   var matches = regex.exec(urlData);
-  return {
-    owner: matches[1],
-    repo: matches[2]
+  if (matches) {
+    return {
+      owner: matches[1],
+      repo: matches[2]
+    };
+  } else {
+    return false;
   }
 }
