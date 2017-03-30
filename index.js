b516abeabbbb5927b6a00452769a5b194ecdb0cd
tim5046 <tim5046@gmail.com>
2017-03-30 08:31:30 -0700

    finish wrapper for auth and commits

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-var gitApi = require('./lib/gitApi');
+var apikey = process.env.githubKey;
 
-gitApi.GitApi();
+var GithubWrapper = require('./lib/GithubWrapper');
+var githubWrapper = new GithubWrapper(apikey);
+
+githubWrapper.authenticate();
+
+githubWrapper.getCommits({
+  owner: 'tim5046',
+  repo: 'assignment_githuh'
+}, function(err, res){
+    res.data.forEach(function(commit){
+      console.log(Object.keys(commit))
+    })
+      console.log('Response: ' + res.data);
+});
+
+
+// var Github = require('github');
+// var github = new Github;
+
+
+// github.authenticate({
+//   type: "token",
+//   token: apikey
+//   })
+
 
 
 
