885dcc1c1e49a452384cebfe540ca95ef68f5c75
DHail <dhail@ymail.com>
2017-03-30 08:03:02 -0700

    working on github wrapper

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-var apikey = process.env.githubKey;
-var Github = require('github');
-var github = new Github;
+var gitApi = require('./lib/gitApi');
 
-github.repos.getCommits(
-  {
-    owner: 'tim5046',
-    repo: 'assignment_githuh'
-  }, function(err, res){
-    res.data.forEach(function(wtf){
-      console.log(Object.keys(wtf))
-    })
-      console.log('Response: ' + res.data);
-  }
-)
+gitApi.GitApi();
+
+
+
