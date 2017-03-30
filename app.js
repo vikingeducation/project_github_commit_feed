7afe1b42ce90880f893091401aa3d3324bcd176e
Renzo Tomlinson <rttomlinson@gmail.com>
2017-03-30 18:00:28 +0000

    Add functionality to access query params:

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const path = require("path");
 
 //Make app instance
 let app = require('./expressish')();
+//Load commits.json data
+let commits = require('./data/commits.json');
+//Turn commits JSON to string and make it pretty
+let prettyCommits = JSON.stringify(commits, null, 2);
+app.setCommitData(prettyCommits);
+
 
 
 //Set port
@@ -14,7 +20,7 @@ let port = process.env.PORT || 3000;
 
 
 //Create new server
-let server = http.createServer(app);
+let server = http.createServer(app.requestHandler);
 
 
 //Start listening on server
@@ -31,8 +37,8 @@ let commitsConfig = {
 
 let pro = github.repos.getCommits(commitsConfig);
 pro.then(function onFulfilled(data) {
-    console.log(data);
+    //console.log(data);
 })
 .catch(function onError(err) {
-    console.log(err);
+    //console.log(err);
 });
--- expressish.js
+++ expressish.js
@@ -2,10 +2,20 @@
 
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 
 
 let app = function () {
-    return function(req, res) {
+    //App instance current commitData
+    let _appCommitData = '';
+    //Object that will hold functions used by the application
+    let api = {};
+    //Allows app to set commitData
+    api.setCommitData = function setCommitData(commitData) {
+        _appCommitData = commitData;
+    };
+    //The app request handler
+    api.requestHandler = function(req, res) {
         //Need to be able to configure the path from the app.js file
         let path = './public/index.html';
         let encoding = 'utf8';
@@ -20,12 +30,22 @@ let app = function () {
             };
         }
         
+        //Parse the query string of the submitted form
+        console.log(req.url);
+        let queryParams = url.parse(req.url, true).query;
+        console.log(queryParams);
+        
+        
+        
         //Read index.html file of public directory
         let p = new Promise(function (resolve, reject) {
             let cb = promiseWrap(resolve, reject);
             fs.readFile(path, {"encoding": encoding}, cb);
         });
         p.then(function onFulfilled(data) {
+            //Replace placeholder with the commitData returned from the server
+            data = data.replace(/{{ commitFeed }}/, _appCommitData);
+            
             res.end(data);
         })
         .catch(function onError(err) {
@@ -34,6 +54,7 @@ let app = function () {
         
         
     };
+    return api;
     
 };
 
