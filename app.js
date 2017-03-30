185c0f639bdc4533868831b0d71eaf106c1d95a4
nicoasp <nicolas.amaya@gmail.com>
2017-03-31 00:55:40 +0200

    add write webhooks data

--- app.js
+++ app.js
@@ -65,15 +65,47 @@ function _writePostToFile(req, done) {
     body = body.substring(8);
     body = JSON.parse(body);
     body = JSON.stringify(body, null, 2);
-    console.log(body);
+    usefulData = [{
+      username: body.pusher.name,
+      repo: body.repository.name
+    }];
+    return readFilePromise('./data/commits.json', usefulData)
+    .then(function(fileDataAndStringifiedData) {
+      return writeFilePromise(
+        './data/commits.json',
+        fileDataAndStringifiedData
+      );
+    })
+    .then(function(){
+      done();
+    });
 
     //fs.writeFile('/data/commits.json', )
     //Need to decode url info
     
-    done();
   });
 }
 
 server.listen(port, host, () => {
   console.log('Server listening at ' + host + ':' + port);
 });
+
+function readFilePromise(path, scrubbedData) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(path, 'utf8', function(err, data) {
+      data = JSON.parse(data);
+      scrubbedData = scrubbedData.concat(data.data);
+      let dataObj = { data: scrubbedData };
+      dataObj = JSON.stringify(dataObj);
+      return resolve(dataObj);
+    });
+  });
+}
+
+function writeFilePromise(path, data) {
+  return new Promise(function(resolve, reject) {
+    fs.writeFile(path, data, 'utf8', function(err) {
+      return resolve();
+    });
+  });
+}
