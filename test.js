20a480647e16ffdccaeeff6abbb7a26b0246cd06
William Whitworth <william10charles@gmail.com>
2017-03-30 14:05:02 -0600

    deleted files?

--- test.js
+++
@@ -1,8 +0,0 @@
-var github_wrapper = require("./github_wrapper");
-
-github_wrapper.getCommits(
-  { owner: "William-Charles", repo: "assignment_node_dictionary_reader" },
-  function(data) {
-    console.log(data);
-  }
-);
