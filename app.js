ae822d7145906ae7a46b7aa637c8dc5ad79dbccf
Egle Libby <eglelibby@gmail.com>
2017-03-30 13:03:07 -0400

    serving commit

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
 const http = require("http");
 const fs = require("fs");
-const router = require("./router");
 const commits = require("./data/commits");
-
+const url = require('url');
+var github_wrapper = require("./github_wrapper");
 const hostname = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+  let info_obj = parseUrl(req.url);
   res.setHeader("Content-Type", "text/html");
   fs.readFile("./public/index.html", (err, data) => {
     if (err) {
       throw err;
     }
-    let regex = /{{commitFeed}}/;
-    res.end(data.toString().replace(regex, JSON.stringify(commits, null, 2)));
+
+    github_wrapper.getCommits(info_obj, function(userCommits) {
+      let regex = /{{commitFeed}}/;
+      res.end(data.toString().replace(regex, JSON.stringify(userCommits, null, 2)));
+    })
   });
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+
+function parseUrl(urlData) {
+  let regex = /user=(.+)&repo=(.+)/
+ 
+  var matches = regex.exec(urlData);
+  return {
+    owner: matches[1],
+    repo: matches[2]
+  }
+}
--- router.js
+++ router.js
@@ -1,19 +1,19 @@
-var url = require('url');
+// var url = require('url');
 
-let Router.routes = {};
+// let Router.routes = {};
 
 
-Router.handle = (req, res) => {
+// Router.handle = (req, res) => {
 
-  var method = req.method.toLowerCase();
-  var path = url.parse(req.url).pathname;
-  console.log(path);
-  if (Router.routes[method][path]) {
-    Router.routes[method][path](req, res);
-  } else {
-    res.statusCode = 404;
-    res.end('404 Not Found');
-  }
-};
+//   var method = req.method.toLowerCase();
+//   var path = url.parse(req.url).pathname;
+//   console.log(path);
+//   if (Router.routes[method][path]) {
+//     Router.routes[method][path](req, res);
+//   } else {
+//     res.statusCode = 404;
+//     res.end('404 Not Found');
+//   }
+// };
 
-module.exports = Router;
+// module.exports = Router;
