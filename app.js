62942be442f66e6e8142aa0392046ff0be609e13
William Whitworth <william10charles@gmail.com>
2017-03-30 15:58:53 -0600

    Functional project

--- app.js
+++ app.js
@@ -18,38 +18,33 @@ const server = http.createServer((req, res) => {
     if (err) {
       throw err;
     } else if (req.url === "/github/webhooks") {
-      
-      let body = '';
-    req.on("data", data => {
-      body += data.toString();
-    });
-    req.on("end", () => {
-      body = JSON.parse(body);
-      console.log('body ', body)
-      let info_obj = {owner: body.pusher.name, repo: body.repository.name};
-      console.log('info_obj ', info_obj)
-      github_wrapper.getCommits(info_obj, function(userCommits) {
-        let scrubbedData = scrubTheData(userCommits);
-        console.log('scrubbed data ', scrubbedData)
-        res.end(
-          // data.replace(regex, JSON.stringify(scrubbedData, null, 2))
-        );
+      let body = "";
+      req.on("data", data => {
+        body += data.toString();
       });
-    });
-
-    }
-
-
-    else if (!info_obj) {
-      res.end(
-        data.toString().replace(regex, commits)
-      );
+      req.on("end", () => {
+        body = JSON.parse(body);
+        console.log("body ", body);
+        let info_obj = { owner: body.pusher.name, repo: body.repository.name };
+        console.log("info_obj ", info_obj);
+        github_wrapper.getCommits(info_obj, function(userCommits) {
+          fs.writeFile("./data/commits.json", JSON.stringify(userCommits));
+          var _headers = {
+            "Content-Type": "text/html",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE"
+          };
+          res.writeHead(200, _headers);
+          res.end("200 OK");
+        });
+      });
+    } else if (!info_obj) {
+      res.end(data.toString().replace(regex, JSON.stringify(commits, null, 2)));
     } else {
       github_wrapper.getCommits(info_obj, function(userCommits) {
-        console.log('info_obj from form submit ', info_obj)
-        let scrubbedData = scrubTheData(userCommits);
         res.end(
-          data.toString().replace(regex, JSON.stringify(scrubbedData, null, 2))
+          data.toString().replace(regex, JSON.stringify(userCommits, null, 2))
         );
       });
     }
@@ -59,20 +54,6 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-function scrubTheData(data) {
-  let scrubArray = [];
-  let commitsArray = data.data;
-  commitsArray.forEach(commit => {
-    let scrubObj = {};
-    scrubObj.sha = commit.sha;
-    scrubObj.commit_message = commit.commit.message;
-    scrubObj.author = commit.commit.author;
-    scrubObj.html_url = commit.html_url;
-    scrubArray.push(scrubObj);
-  });
-  return scrubArray;
-}
 
 function parseUrl(urlData) {
   let regex = /user=(.+)&repo=(.+)/;
--- github_wrapper.js
+++ github_wrapper.js
@@ -11,8 +11,23 @@ var github_wrapper = {};
 
 github_wrapper.getCommits = function(info_obj, callback) {
   github.repos.getCommits(info_obj).then(function(data) {
+    data = scrubTheData(data);
     callback(data);
   });
 };
+
+function scrubTheData(data) {
+  let scrubArray = [];
+  let commitsArray = data.data;
+  commitsArray.forEach(commit => {
+    let scrubObj = {};
+    scrubObj.sha = commit.sha;
+    scrubObj.commit_message = commit.commit.message;
+    scrubObj.author = commit.commit.author;
+    scrubObj.html_url = commit.html_url;
+    scrubArray.push(scrubObj);
+  });
+  return scrubArray;
+}
 
 module.exports = github_wrapper;
