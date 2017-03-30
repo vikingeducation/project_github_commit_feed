6de80471c22def65b4f8de30dc1942a7c72a2a48
Jerry Gao <gao@blackwright.com>
2017-03-30 13:02:15 -0400

    link gitWrapper to server

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+const github = require('./lib/gitWrapper');
 const json = require('./data/commits.json');
 
 const port = 3000;
@@ -12,15 +13,24 @@ let reqQuery;
 const server = http.createServer( (req, res) => {
   let path = req.url;
 
+  console.log(path);
+
   if (path.match(/^\/commits\?.+/)) {
     reqQuery = url.parse(path,true).query;
+    console.log(reqQuery);
   }
 
-  fs.readFile('./public/index.html', 'utf-8', (err, data) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    data = data.replace('{{ commitFeed }}', JSON.stringify(json, null, 2));
-    res.end(data);
-  });
+  if (reqQuery) {
+    const gitData = github.gitCommits(reqQuery.user, reqQuery.repo);
+    
+    gitData.then( (resolvedData) => {
+      fs.readFile('./public/index.html', 'utf-8', (err, data) => {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        data = data.replace('{{ commitFeed }}', JSON.stringify(resolvedData, null, 2));
+        res.end(data);
+      });
+    });
+  }
 });
 
 server.listen(port, host, () => {
