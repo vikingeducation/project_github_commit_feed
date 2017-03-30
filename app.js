e62a6e525e0dab566a8eef9038e75ca54b731ac8
nicoasp <nicolas.amaya@gmail.com>
2017-03-30 18:29:06 +0200

    parse form data

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const fs = require('fs');
 const host = 'localhost';
 const port = '3000';
 const jsonData = require('./data/commits.json');
+const url = require('url');
 
 
 let userInfo = githubWrapper.setUserInfo('nicoasp', 'assignment_js_sprint');
 githubWrapper.getCommits(userInfo).then(data => {
-  console.log(data);
+  // console.log(data);
 });
 
 //For testing: ngrok http 3000
 
 let server = http.createServer(function(req, res) {
+  let pathname = url.parse(req.url).pathname;
+  let params = url.parse(req.url, true).query;
+
   fs.readFile('./public/index.html', 'utf8', function(err, data) {
     if (err) {
       res.statusCode('404');
