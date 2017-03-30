d3def896ea3527bd69ec31ae9ea73e89bb772a73
Jerry Gao <gao@blackwright.com>
2017-03-30 12:01:35 -0400

    start form parsing

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
+
+const json = require('./data/commits.json');
 
 const port = 3000;
 const host = 'localhost';
 
 const server = http.createServer( (req, res) => {
+
+  let reqQuery = url.parse(req.url).query;
+  
+
   fs.readFile('./public/index.html', 'utf-8', (err, data) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    data = data.replace('{{ commitFeed }}', JSON.stringify(json, null, 2));
     res.end(data);
   });
 });
