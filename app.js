18713a52563208e130542ee43d08d4bcaf71f585
Christian Florez <florez.christian@gmail.com>
2017-05-18 12:52:36 -0400

    parse username and repo

--- app.js
+++ app.js
@@ -1,46 +1,44 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 let json = require('./data/commits');
 
 const hostname = 'localhost';
 const port = 3001;
 
-const _extractPostData = (req, done) => {
-  var body = '';
-  console.log(body);
-  console.log('hi');
-  req.on('data', (data) => {
-    body += data;
-  });
-  req.on('end', () => {
-    req.body = body;
-    done();
-  });
+const getParams = (query) => {
+  let usernameRegEx = /username=([^&]*)/;
+  let repoRegEx = /repo=([^&]*)/;
+
+  let username = query.match(usernameRegEx);
+  let repo = query.match(repoRegEx);
+
+  let results = {
+    username: username[1],
+    repo: repo[1]
+  };
+
+  return results;
 };
 
 const handleRouting = (req, res) => {
   res.statusCode = 200;
   let method = req.method.toLowerCase();
+  const query = url.parse(req.url).query;
 
-  var p = new Promise((resolve) => {
-      if (method === 'post') {
-        _extractPostData(req, resolve);
-      } else {
-        resolve();
-      }
-  });
+  if (query) {
+    let params = getParams(query);
+  }
+  
+  fs.readFile('./public/index.html', 'utf8', (err, file) =>{
+    if (err) throw err;
 
-  p.then(()=>{
-    fs.readFile('./public/index.html', 'utf8', (err, file) =>{
-      if (err) throw err;
+    let feed = JSON.stringify(json, null, 2);
+    file = file.replace('{{ commitFeed }}', feed);
 
-      let feed = JSON.stringify(json, null, 2);
-      file = file.replace('{{ commitFeed }}', feed);
-
-      res.setHeader('Content-Type', 'text/html');
-      res.write(file);
-      res.end();
-    });
+    res.setHeader('Content-Type', 'text/html');
+    res.write(file);
+    res.end();
   });
 };
 const server = http.createServer(handleRouting);
