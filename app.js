be526112a15dec53c8c8e9d16961c835b6cd0be2
Malyar Baron <malbaron0@gmail.com>
2017-06-03 19:23:44 -0400

    Displaying Dynamic JSON in HTML

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let http = require('http');
 let fs = require('fs');
-
+let commitFeed = require('./data/commits.json');
 
 let port = process.env.PORT || process.argv[2] || 3000;
 let host = "localhost";
@@ -10,14 +10,26 @@ const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
 
-        fs.createReadStream('./public/index.html').pipe(res);
-
+        renderHTML(res);
     }
-    else{
+    else {
         res.statusCode = 404;
         res.end('not found');
     }
 });
+
+
+//read the html file into res.write and insert the appropriate JSON commit by replacing {{commitFeed}} in the html file
+let renderHTML = (res) => {
+    let commitFeedString = JSON.stringify(commitFeed, null, 2);
+
+    fs.readFile('./public/index.html', 'utf8', (err, data) => {
+        if (err) throw err;
+        data = data.replace('{{commitFeed}}', commitFeedString);
+        res.write(data);
+        res.end();
+    });
+}
 
 
 server.listen(port, host, () => {
