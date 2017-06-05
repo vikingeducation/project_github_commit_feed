454ffda85967970ad1e7bf2b9c51ccec709f9fca
Malyar Baron <malbaron0@gmail.com>
2017-06-04 20:21:22 -0400

    Listening to all request URLS

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ const server = http.createServer((req, res) => {
         getCommits(getUserAndRepo(req, res));
         res.end();
     }
-    else {
-        res.statusCode = 404;
-        res.end('not found');
+    else{
+        res.statusCode = 200;
+        res.end(`You made a ${req.method} request`);
     }
 });
 
