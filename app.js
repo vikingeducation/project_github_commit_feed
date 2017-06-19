48af71a992d1105898d1700af642555e463b540d
Tyler Ketron <tketron@gmail.com>
2017-06-18 19:47:35 -0700

    parse url parameters on form submit

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 var commitFeedJSON = require('./data/commits.json');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  var queryParams, path, parsedQuery;
+  var path = url.parse(req.url).query;
+
+  parsedQuery = path.split(/(=|&)/g);
+  queryParams = {
+    user: parsedQuery[2],
+    repo: parsedQuery[6]
+  };
+  console.log(queryParams);
+
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   fs.readFile('public/index.html', 'utf8', (err, data) => {
