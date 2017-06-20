76169de307896050e36e76c6a2039d4ad19293ce
Tyler Ketron <tketron@gmail.com>
2017-06-19 21:08:59 -0700

    completed update of index file using query parameters

--- app.js
+++ app.js
@@ -10,38 +10,49 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   var queryParams, path, parsedQuery;
   var path = url.parse(req.url).query;
+  if (path) {
+    parsedQuery = path.split(/(=|&)/g);
+    queryParams = {
+      user: parsedQuery[2],
+      repo: parsedQuery[6]
+    };
 
-  parsedQuery = path.split(/(=|&)/g);
-  queryParams = {
-    user: parsedQuery[2],
-    repo: parsedQuery[6]
-  };
-  console.log(queryParams);
+    githubWrapper.getCommits(queryParams.user, queryParams.repo)
+      .then( data => {
+        var formattedCommitData = data.data.map(function(commit) {
+          var formattedCommit = {};
+          formattedCommit.author = commit.commit.author;
+          formattedCommit.message = commit.commit.message;
+          formattedCommit.html_url = commit.html_url;
+          formattedCommit.sha = commit.sha;
+          return formattedCommit;
+        });
 
-  githubWrapper.getCommits(queryParams.user, queryParams.repo)
-    .then( data => {
-      var formattedCommitData = data.data.map(function(commit) {
-        var formattedCommit = {};
-        formattedCommit.author = commit.commit.author;
-        formattedCommit.message = commit.commit.message;
-        formattedCommit.html_url = commit.html_url;
-        formattedCommit.sha = commit.sha;
-        return formattedCommit;
-      });
-      console.log(JSON.stringify(formattedCommitData));
-      fs.writeFile('./data/commits.json', JSON.stringify(formattedCommitData), (err) => {
-        if (err) throw err;
-        console.log('Commit file updated');
-      });
-    })
-    .catch(err => console.log(err));
+        fs.writeFile('./data/commits.json', JSON.stringify(formattedCommitData, null, 2), (err) => {
+          if (err) throw err;
+          console.log('Commit file updated');
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  fs.readFile('public/index.html', 'utf8', (err, data) => {
-    if (err) return res.end(err);
-    res.end(data.replace('{{commitFeed}}', JSON.stringify(commitFeedJSON, null, 2)));
-  });
+          fs.readFile('public/index.html', 'utf8', (err, data) => {
+            if (err) return res.end(err);
+            fs.readFile('./data/commits.json', 'utf8', (err, json) => {
+              if (err) throw err(err);
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'text/html');
+              res.end(data.replace('{{commitFeed}}', json));
+            });
+          });
+        });
+      })
+      .catch(err => console.log(err));
+  } else {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    fs.readFile('public/index.html', 'utf8', (err, data) => {
+      if (err) return res.end(err);
+      res.end(data);
+      res.end(data.replace('{{commitFeed}}', JSON.stringify(commitFeedJSON, null, 2)));
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
