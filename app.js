312e830a12b448a511d45068ca6cc1b669782c1a
Graham Turner <turnertgraham@gmail.com>
2017-07-09 14:00:47 -0400

    server from commits file

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var http = require('http');
 var fs = require('fs');
+var commits = require('./data/commits.json');
 
 var server = http.createServer((req, res) => {
-  var file = fs.readFileSync('./public/index.html');
+  var fileString = fs.readFileSync('./public/index.html').toString('utf8');
+  var commitsString = JSON.stringify(commits, null, 2);
+  var edited = fileString.replace(/{{ commitFeed }}/, commitsString);
   res.writeHead(200, {contentType: 'application/html'});
-  res.end(file);
+  res.end(edited);
 });
 
 server.listen(3000, 'localhost', (() => {
