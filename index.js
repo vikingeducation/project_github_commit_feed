12ac80d1b4459022cb04506834e70b89486e63e8
Graham Turner <turnertgraham@gmail.com>
2017-07-09 13:36:42 -0400

    working wrapper

--- index.js
+++ index.js
@@ -1,26 +1,7 @@
-var GithubApi = require('github');
-var secrets = require("./secrets.json")
+var wrapper = require('./lib/github_wrapper');
 
-var github = new GithubApi({
-  // optional
-  protocol: "https",
-  host: "api.github.com", // should be api.github.com for GitHub
-  headers: {
-      "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
-  },
-  timeout: 5000
-});
-
-github.authenticate({
-    type: "token",
-    token: secrets.github,
-});
-
-var commits = github.repos.getCommits({
-  owner: 'tgturner',
-  repo: 'kithub'
-});
+var commits = wrapper.getCommits('tgturner', 'kithub');
 
 commits.then((data) => {
   console.log(data)
-})
+});
