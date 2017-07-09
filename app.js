4e018324ae8792d620b89f2b09a0657257f26632
Graham Turner <turnertgraham@gmail.com>
2017-07-09 14:10:59 -0400

    parse it

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 var http = require('http');
 var fs = require('fs');
 var commits = require('./data/commits.json');
+var url = require('url');
+
+var extractInfo = (path) => {
+  var pathObj = url.parse(path);
+  var lastVal = pathObj.pathname.split('/')[1];
+  if (lastVal == 'commits') {
+    var matches = pathObj.query.match(/user=(.*)&repo=(.*)$/)
+    var user = matches[1];
+    var repo = matches[2];
+
+    console.log(user);
+    console.log(repo);
+  }
+}
 
 var server = http.createServer((req, res) => {
+  extractInfo(req.url);
   var fileString = fs.readFileSync('./public/index.html').toString('utf8');
   var commitsString = JSON.stringify(commits, null, 2);
   var edited = fileString.replace(/{{ commitFeed }}/, commitsString);
