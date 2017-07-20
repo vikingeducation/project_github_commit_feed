5e5f2f2b00d6746602a0bf8996e134e762f06ea6
Ian Halverson <halversoncode@gmail.com>
2017-07-20 16:44:14 -0400

    trying to render

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ const http = require("http");
 const url = require("url");
 const app = require("./lib/routeInit.js");
 const apiWrapper = require("./lib/apiWrapper");
+const render = require("./lib/render");
 
 const PORT = process.env.port || process.argv[2] || 3000;
 const HOST = "127.0.0.1";
 
 app.get("/", (req, res) => {
   res.statusCode = 200;
-  res.end("Hello World!");
 });
 
 app.get("/:username/:repository", (req, res) => {
@@ -17,11 +17,10 @@ app.get("/:username/:repository", (req, res) => {
 
   apiWrapper.getCommits().then(apiWrapper.parseData).then(
     results => {
-      console.log(results);
       res.statusCode = 200;
-
+      render("index.html", results).then().catch();
+      res.end("hi");
       // read in the file we need to display (index.html)
-      res.end();
 
       // res.render('index', {
       //   title: 'Github Commit Feed',
