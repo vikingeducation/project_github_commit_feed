bcf3ca928ae7fd7368119b26d329b00df1e7d79e
Ian Halverson <halversoncode@gmail.com>
2017-07-20 11:09:05 -0400

    parseData function created, but failing

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const Github = require("github");
+const apiWrapper = require("./lib/apiWrapper");
 
-const github = new Github();
-
-github.authenticate({
-  type: "token",
-  token: "32fcf0a1da80eb404cd704aaa5b6c6d490cf991c"
+apiWrapper.getCommits().then(results => {
+  console.log(apiWrapper.parseData(results.data));
 });
-
-var commits = github.repos
-  .getCommits({
-    owner: "idhalverson",
-    repo: "assignment_building_the_express_router"
-  })
-  .then(resolve => {
-    resolve.data.forEach(commit => {
-      console.log(commit.commit);
-    });
-  });
