20423b86df4cd7cfb39e8a1a104e1e5caa081841
Andrew Senner <me@andrewsenner.com>
2017-07-20 13:02:04 -0400

    Unexpressed app

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,17 @@ const apiWrapper = require("../lib/apiWrapper");
 router.get('/', function(req, res, next) {
   apiWrapper.authenticate();
   apiWrapper.getCommits().then(results => {
-    res.render('index', {
-      title: 'Github Commit Feed',
-      commits: apiWrapper.parseData(results.data)
-    });
-  });
+		res.statusCode = 200;
 
-  
+		// read in the file we need to display (index.html)
+
+		res.end();
+
+    // res.render('index', {
+    //   title: 'Github Commit Feed',
+    //   commits: apiWrapper.parseData(results.data)
+    // });
+  });
 });
 
 module.exports = router;
