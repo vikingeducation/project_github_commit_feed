ab3bd44565491155977bb0e588cdb1273833c16f
Ian Halverson <halversoncode@gmail.com>
2017-07-20 18:00:50 -0400

    trying to display errors

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const url = require("url");
 const app = require("./lib/routeInit.js");
 const apiWrapper = require("./lib/apiWrapper");
 const render = require("./lib/render");
-const fs = require('fs');
+const fs = require("fs");
 
 const PORT = process.env.port || process.argv[2] || 3000;
 const HOST = "127.0.0.1";
@@ -18,19 +18,25 @@ app.get("/", (req, res) => {
 app.get("/:username/:repository", (req, res) => {
   // Authenticate with github using our token
   apiWrapper.authenticate();
-
-  apiWrapper.getCommits().then(apiWrapper.parseData).then(
+  console.log(req.params);
+  apiWrapper.getCommits(req.params).then(apiWrapper.parseData).then(
     results => {
       res.statusCode = 200;
-      render("index.html", results).then(result => {
+      render.render("index.html", results).then(result => {
         res.end(result);
       });
-    }, console.log);
+    },
+    error => {
+      render.renderError(error.message).then(result => {
+        res.end(result);
+      });
+    }
+  );
 });
 
 // TODO: NEED TO ACCEPT PATTERNS SO THAT CLIENT CAN REQUEST ANY STYLESHEET
-app.get('/public/stylesheets/style.css', (req, res) => {
-  fs.readFile('./public/stylesheets/style.css', (err, data) => {
+app.get("/public/stylesheets/style.css", (req, res) => {
+  fs.readFile("./public/stylesheets/style.css", (err, data) => {
     if (err) throw err;
     res.writeHead(200, {
       "Content-Type": "text/css"
@@ -40,8 +46,8 @@ app.get('/public/stylesheets/style.css', (req, res) => {
 });
 
 // TODO: NEED TO ACCEPT PATTERNS SO THAT CLIENT CAN REQUEST ANY JAVASCRIPT
-app.get('/public/javascripts/script.js', (req, res) => {
-  fs.readFile('./public/javascripts/script.js', (err, data) => {
+app.get("/public/javascripts/script.js", (req, res) => {
+  fs.readFile("./public/javascripts/script.js", (err, data) => {
     if (err) throw err;
     res.writeHead(200, {
       "Content-Type": "text/javascript"
