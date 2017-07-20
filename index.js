8659cbaddbb61d3c6d2f5e85f8789c4d980d0eee
Andrew Senner <me@andrewsenner.com>
2017-07-20 14:36:33 -0400

    lunch time, fixed some bugs

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const http = require('http');
 const url = require('url');
+const app = require('./lib/routeInit.js');
+
+const PORT = process.env.port ||
+	process.argv[2] || 3000;
+const HOST = "127.0.0.1";
+
+app.get('/', (req, res) => {
+	console.log(req.params);
+
+	// apiWrapper.authenticate();
+  // apiWrapper.getCommits().then(results => {
+  //   res.statusCode = 200;
+	//
+  //   // read in the file we need to display (index.html)
+	//
+  //   res.end();
+	//
+  //   // res.render('index', {
+  //   //   title: 'Github Commit Feed',
+  //   //   commits: apiWrapper.parseData(results.data)
+  //   // });
+  // });
+});
+
+app.listen(PORT, HOST, () => {
+	console.log(`Listening on ${ HOST }:${ PORT }`);
+})
