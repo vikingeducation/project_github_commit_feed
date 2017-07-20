9a1707e1bf4548a5ef9439bf4fa7e6631d69f502
Eric Glover <etgt28@mail.missouri.edu>
2017-07-20 12:03:23 -0500

    working on front-end page stuffz

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-//
+const fs = require("fs");
 let http = require("http");
 let port = process.env.PORT || 3000;
+
 let host = "localhost";
 const user_name = "karathrash";
 const repo = "project_github_commit_feed";
 
 const git = require("./lib/github_wrapper");
-var server = http.createServer((req, res) => {
-  res.end("this can just be a string");
 
-  console.log("server on");
+var server = http.createServer((req, res) => {
+  res.writeHeader(200, { "Content-type": "text/html" });
+  var data = fs.readFileSync("./public/index.html");
+  res.end(data);
+
+  //console.log("server on");
 });
+
 server.listen(port, host, () => {
   console.log(`Listening at: http://${host}:${port}`);
 });
@@ -19,9 +24,9 @@ server.listen(port, host, () => {
 
 git.repos(user_name, repo).then(
   message => {
-    console.log(message);
+    //console.log(message);
   },
   err => {
-    console.log(err);
+    //console.log(err);
   }
 );
