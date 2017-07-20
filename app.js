760d472b5a4626770bc8dbdd555edd36dc8385aa
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-20 13:24:03 -0400

    almost POSTing

--- app.js
+++ app.js
@@ -11,16 +11,27 @@ html = html.replace("{{commitFeed}}", JSON.stringify(commits, null, 2));
 
 const server = http.createServer(function(req, res) {
   var body = "";
- // console.log(req);
-  req.on("data", data => {
-    console.log(data);
-    body += data;
-  });
-  req.on("end", () => {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end(html);
-  });
+  console.log(req.url);
+  console.log(req.method);
+  if (req.method ==='POST') {
+    console.log('a third test string')
+    req.on("data", data => {
+  //    console.log('this is a test string');
+      console.log(data);
+      body += data;
+    })
+
+    req.on("end", () => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      console.log(body)
+      res.end(html);
+    });
+  }
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end(html);
 });
+
+
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
