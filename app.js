7688e3a1f2470a080f672e8fe3219c97596acfff
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-20 16:23:31 +0000

    working on post

--- app.js
+++ app.js
@@ -1,31 +1,27 @@
-const http = require('http');
-const fs = require('fs');
-const commits = require('./data/commits.json')
-
-//console.log(JSON.stringify(commits, null, 2))
-
-// JSON.stringify(commits, null, 2)
-
-
-
-
-const hostname = '127.0.0.1';
+const http = require("http");
+const fs = require("fs");
+const commits = require("./data/commits.json");
+const url = require("url");
+const hostname = "0.0.0.0";
 const port = 3000;
 
-var html = fs.readFileSync('./public/index.html', 'utf8');
+var html = fs.readFileSync("./public/index.html", "utf8");
 
+html = html.replace("{{commitFeed}}", JSON.stringify(commits, null, 2));
 
-html = html.replace('{{commitFeed}}', JSON.stringify(commits, null, 2))
-
-
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(html);
+const server = http.createServer(function(req, res) {
+  var body = "";
+  console.log(req);
+  req.on("data", data => {
+    console.log(data);
+    body += data;
+  });
+  req.on("end", () => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(html);
+  });
 });
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
-
-
