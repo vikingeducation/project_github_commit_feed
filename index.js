bfbb83714e86bf51eb4ffc00bb64a39d9c2c52b3
Kara Thrash <dvdonato@gmail.com>
2017-07-20 12:40:23 -0400

    ngrok works

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 //
+let http = require("http");
+let port = process.env.PORT || 3000;
+let host = "localhost";
 const user_name = "karathrash";
 const repo = "project_github_commit_feed";
 
 const git = require("./lib/github_wrapper");
-git.authenticate();
+var server = http.createServer((req, res) => {
+  res.end("this can just be a string");
+
+  console.log("server on");
+});
+server.listen(port, host, () => {
+  console.log(`Listening at: http://${host}:${port}`);
+});
+
+//git.authenticate();
 
 git.repos(user_name, repo).then(
   message => {
