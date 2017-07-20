d1ea396d14f35708dd487a6a11642326f0408f75
Ian Halverson <halversoncode@gmail.com>
2017-07-20 14:57:14 -0400

    merging

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
-const http = require('http');
-const url = require('url');
-const app = require('./lib/routeInit.js');
+const http = require("http");
+const url = require("url");
+const app = require("./lib/routeInit.js");
 
-const PORT = process.env.port ||
-	process.argv[2] || 3000;
-const HOST = "127.0.0.1";
+const PORT = process.env.port || process.argv[2] || 3000;
+const HOST = "localhost";
 
-app.get('/', (req, res) => {
-	console.log(req.params);
-
-	// apiWrapper.authenticate();
+app.get("/", (req, res) => {
+  // apiWrapper.authenticate();
   // apiWrapper.getCommits().then(results => {
   //   res.statusCode = 200;
-	//
+  //
   //   // read in the file we need to display (index.html)
-	//
+  //
   //   res.end();
-	//
+  //
   //   // res.render('index', {
   //   //   title: 'Github Commit Feed',
   //   //   commits: apiWrapper.parseData(results.data)
@@ -25,5 +22,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, HOST, () => {
-	console.log(`Listening on ${ HOST }:${ PORT }`);
-})
+  console.log(`Listening on ${HOST}:${PORT}`);
+});
