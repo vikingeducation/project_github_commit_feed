623b9068f2ae8185020ea7bff2d823fbe0d0b260
Stephanie Barker <song_mage@yahoo.com>
2017-07-20 16:32:06 -0400

    bluh

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ function listenWebHook(req, res) {
     body = body.slice(8);
 
     body = qs.parse(body);
-    newBody = JSON.stringify(body);
+    newBody = JSON.stringify(body, null, 2);
 
     console.log(newBody);
 
