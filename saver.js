ffc0379704ce24b1a5993c219a7c74002cfdf228
EdTriplett <EdTriplett@users.noreply.github.com>
2017-07-20 18:01:49 -0400

    saving commits

--- saver.js
+++ saver.js
@@ -1,7 +1,19 @@
-let fs = require("fs");
+const fs = require("fs");
+
 
 let saver = input => {
-  fs.appendFile("./data/commits.json", input, (err){
+  fs.readFile('./data/commits.json', (err, data) => {
     if (err) throw err;
-  });
+    console.log(data);
+    let temp = JSON.parse(data);
+    let tempArr = []
+    tempArr.push(temp);
+    tempArr.push(input);
+    fs.writeFile("./data/commits.json", JSON.stringify(tempArr, null, 2), (err) => {
+      if (err) throw err;
+ //     console.log('test');
+     });
+  })
+  ;
 };
+ module.exports = saver
--- wrapper.js
+++ wrapper.js
@@ -1,4 +1,6 @@
-let Github = require("github");
+const Github = require("github");
+const saver = require("./saver.js");
+
 
 const github = new Github({ debug: true });
 
@@ -17,16 +19,18 @@ module.exports = {
         repo: rep
       },
       function(err, res) {
-        console.log(res.data[0].sha);
-        console.log(res.data[0].commit.author);
-        console.log(res.data[0].commit.message);
-        console.log(res.data[0].commit.url);
+//        console.log(res.data[0].sha);
+  //      console.log(res.data[0].commit.author);
+    //    console.log(res.data[0].commit.message);
+      //  console.log(res.data[0].commit.url);
         let results = {
           sha: res.data[0].sha,
           author: res.data[0].commit.author,
           message: res.data[0].commit.message,
           url: res.data[0].commit.url
         };
+        console.log(results);
+        saver(results);
       }
     );
   }
