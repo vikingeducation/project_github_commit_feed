7c81412c301539b1b5da39e8cbd4305828d91dc5
Greg Filipczak <gregfilipczak@gmail.com>
2017-07-20 21:57:16 +0000

    completed web interface

--- saver.js
+++ saver.js
@@ -1,19 +1,19 @@
 const fs = require("fs");
 
-
 let saver = input => {
-  fs.readFile('./data/commits.json', (err, data) => {
+  fs.readFile("./data/commits.json", (err, data) => {
     if (err) throw err;
-    console.log(data);
     let temp = JSON.parse(data);
-    let tempArr = []
+    let tempArr = [];
     tempArr.push(temp);
     tempArr.push(input);
-    fs.writeFile("./data/commits.json", JSON.stringify(tempArr, null, 2), (err) => {
-      if (err) throw err;
- //     console.log('test');
-     });
-  })
-  ;
+    fs.writeFile(
+      "./data/commits.json",
+      JSON.stringify(tempArr, null, 2),
+      err => {
+        if (err) throw err;
+      }
+    );
+  });
 };
- module.exports = saver
+module.exports = saver;
--- wrapper.js
+++ wrapper.js
@@ -1,7 +1,6 @@
 const Github = require("github");
 const saver = require("./saver.js");
 
-
 const github = new Github({ debug: true });
 
 module.exports = {
@@ -19,10 +18,6 @@ module.exports = {
         repo: rep
       },
       function(err, res) {
-//        console.log(res.data[0].sha);
-  //      console.log(res.data[0].commit.author);
-    //    console.log(res.data[0].commit.message);
-      //  console.log(res.data[0].commit.url);
         let results = {
           sha: res.data[0].sha,
           author: res.data[0].commit.author,
