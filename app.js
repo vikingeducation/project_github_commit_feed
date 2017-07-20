e6d9fa1b3e09e666d638211dc6b25a9772d1cba5
Stephanie Barker <song_mage@yahoo.com>
2017-07-20 11:09:05 -0400

    trying to replace commitFeed with json file

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const http = require("http");
 const fs = require("fs");
+var commitFeed = require("./data/commits");
+
+commitFeed = JSON.stringify(commitFeed, null, 2);
 
 const server = http.createServer(function(req, res) {
 	res.setHeader('Content-Type', 'text/html');
 
 	fs.readFile("./public/index.html", "utf8", function(err, data) {
 		if (err) throw err;
+
+		data.replace("{{ commitFeed }}", commitFeed);
 		res.end(data);
 	});
 });
