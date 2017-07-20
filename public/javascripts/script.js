4d34f8e796d701f7cde5d8b1a33cfd26aecbb8e1
Ian Halverson <halversoncode@gmail.com>
2017-07-20 18:59:09 -0400

    almost done

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,26 +1,46 @@
 $(document).ready(function() {
-	$('form').on('submit', function(e) {
-		// Fetch input values.
-		let username = $('input#username').val();
-		let repository = $('input#repository').val();
+  $("form").on("submit", function(e) {
+    // Fetch input values.
+    let username = $("input#username").val();
+    let repository = $("input#repository").val();
 
-		window.location = `/${username}/${repository}`;
+    window.location = `/${username}/${repository}`;
 
-		e.preventDefault();
-		return true;
-	});
+    e.preventDefault();
+    return true;
+  });
 
-	gatherCommitNames();
+  gatherCommitNames().then(insertOptions).change(filterByName);
 });
 
 function gatherCommitNames() {
-	let names = [];
-	$('.commit-name').each(function(idx, el) {
-		const $el = $(el);
-		const name = $el[0].innerText.slice(6);
-		if (!names.includes(name))
-			names.push(name);
-	});
+  return new Promise(resolve => {
+    let names = [];
+    $(".commit-name").each(function(idx, el) {
+      const $el = $(el);
+      const name = $el[0].innerText.slice(6);
+      if (!names.includes(name)) names.push(name);
+    });
+    resolve(names);
+  });
+}
 
-	console.log(names);
+function insertOptions(names) {
+  var $menuFilter = $("#name-filter");
+  names.forEach(function(name) {
+    var $element = $("<option/>", { value: name, text: name });
+    $menuFilter.append($element);
+  });
+  return $menuFilter;
+}
+
+function filterByName(e) {
+  var $target = $(e.target);
+  console.log($target);
+  var $commits = $("commit");
+  $commits.show();
+  $commits.each(function(idx, commit) {
+    //var name = commit.("name");
+    // if (name === )
+  });
 }
