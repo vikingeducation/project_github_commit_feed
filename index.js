d7db8904efa045b1dec569ae513eae07c1e687a7
Eric Glover <etgt28@mail.missouri.edu>
2017-07-20 11:13:36 -0500

    api wrapper complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-//take input from github wrapper
+//
+const user_name = "karathrash";
+const repo = "project_github_commit_feed";
+
+const git = require("./lib/github_wrapper");
+git.authenticate();
+
+git.repos(user_name, repo).then(
+  message => {
+    console.log(message);
+  },
+  err => {
+    console.log(err);
+  }
+);
