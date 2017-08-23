9de51dd27604149abade16bd2c43fa1008d8c7db
Chuck Michael <chuckqs@yahoo.com>
2017-08-23 14:28:16 -0400

    Displaying Dynamic Json completed

--- modules/routes.js
+++ modules/routes.js
@@ -7,10 +7,21 @@ routes.get = (path, callback) => {
 };
 
 routes.get('/', function (req,res){
-  fs.readFile('./public/index.html', function(err,data){
-    res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length':data.length});
-    res.write(data);
-    res.end();
+  fs.readFile('./public/index.html', "utf8", function(err,data){
+    if(err){
+      throw err;
+    }
+    fs.readFile('./data/commits.json', "utf8", function(err2, data2){
+      if (err2) {
+        throw err2;
+      }
+      var stringedData = data.toString();
+      data  = stringedData.replace( '{{ commitFeed }}', data2);
+
+      res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length':data.length});
+      res.write(data);
+      res.end();
+    })
   })
 })
 
