ce0e6eb0a9eb08224f64e4e190eb407554e82e28
Chuck Michael <chuckqs@yahoo.com>
2017-08-23 19:09:45 -0400

    webhook test

--- modules/routes.js
+++ modules/routes.js
@@ -59,7 +59,7 @@ var routesInit = function () {
   })
 
   routes.post('/', function (req,res){
-    console.log( req.data);
+    console.log( req);
     var _headers = {
       "Content-Type": "text/html",
       "Access-Control-Allow-Origin": "*",
