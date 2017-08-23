782561ca46d553e4d5b5e515ff11f51e8e40ab3d
Chuck Michael <chuckqs@yahoo.com>
2017-08-23 17:46:04 -0400

    Parsed form data

--- modules/routes.js
+++ modules/routes.js
@@ -1,37 +1,60 @@
 var fs = require('fs');
+var github = require('../lib/github_wrapper');
 
-var routes = {};
 
-routes.get = (path, callback) => {
-  routes.get[path] = callback;
-};
 
-routes.get('/', function (req,res){
-  fs.readFile('./public/index.html', "utf8", function(err,data){
-    if(err){
-      throw err;
-    }
-    fs.readFile('./data/commits.json', "utf8", function(err2, data2){
-      if (err2) {
-        throw err2;
+var routesInit = function () {
+  var routes = {};
+
+  routes.get = (path, callback) => {
+    routes.get[path] = callback;
+  };
+
+  routes.get('/', function (req,res){
+    fs.readFile('./public/index.html', "utf8", function(err,data){
+      if(err){
+        throw err;
       }
-      var stringedData = data.toString();
-      data  = stringedData.replace( '{{ commitFeed }}', data2);
+      fs.readFile('./data/commits.json', "utf8", function(err2, data2){
+        if (err2) {
+          throw err2;
+        }
+        var stringedData = data.toString();
+        data  = stringedData.replace( '{{ commitFeed }}', data2);
 
-      res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length':data.length});
+        res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length':data.length});
+        res.write(data);
+        res.end();
+      })
+    })
+  })
+
+  routes.get('/stylesheets/bootstrap.css', function (req,res){
+    fs.readFile('./public/stylesheets/bootstrap.min.css', function(err,data){
+      res.writeHead(200, {'Content-Type': 'text/css',  'Content-Length':data.length});
       res.write(data);
       res.end();
     })
   })
-})
 
-routes.get('/stylesheets/bootstrap.css', function (req,res){
-  fs.readFile('./public/stylesheets/bootstrap.min.css', function(err,data){
-    res.writeHead(200, {'Content-Type': 'text/css',  'Content-Length':data.length});
-    res.write(data);
-    res.end();
+
+  routes.get('/commits', function (req,res){
+    var regex = /=(\w+)/g;
+    var matchs = (req.url).match(regex);
+    if (matchs != null) {
+      var user = matchs[0].slice(1);
+      var repo = matchs[1].slice(1);
+      github(user, repo , function () {
+        routes.get['/'](req,res);
+      });
+
+    }
+    else {
+      routes.get['/'](req,res);
+    }
   })
-})
+  return routes;
+}
 
 
-module.exports = routes;
+module.exports = routesInit();
