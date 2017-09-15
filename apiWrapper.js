fbdf0ea907f05aa30ab4cde912fa828f59fd41df
Coelacanth7 <galford151@gmail.com>
2017-09-15 14:23:02 -0700

    set up a wrapper

--- apiWrapper.js
+++ apiWrapper.js
@@ -1,6 +1,21 @@
 const token = require("./apikey");
 var GithubApi = require("github");
-
 var github = new GithubApi({});
 
-module.exports = github;
+const apiWrapper = {
+	authenticate: () => {
+		github.authenticate({
+			type: "token",
+			token: token
+		});
+	},
+
+	returnCommits: (owner, repo) => {
+		github.repos.getCommits({
+			owner: owner,
+			repo: repo
+		});
+	}
+};
+
+module.exports = apiWrapper;
