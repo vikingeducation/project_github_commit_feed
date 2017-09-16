7d0c2f4366ad598020e5970dfbda26f3b5172a11
Coelacanth7 <galford151@gmail.com>
2017-09-15 23:23:20 -0700

    organized a few things

--- app.js
+++ app.js
@@ -11,39 +11,38 @@ var port = process.env.PORT || process.argv[2] || 3000;
 var host = "localhost";
 
 var server = http.createServer((req, res) => {
+	handle(req, res);
+});
+
+function handle(req, res) {
 	var reqUrl = req.url;
 	var urlMatchesFormSubmit = urlRegex.test(req.url);
-	var urlParsed = url.parse(reqUrl, true).query;
+	var urlParsedObj = url.parse(reqUrl, true).query;
 
 	console.log("urlMatchesFormSubmit " + urlMatchesFormSubmit);
 	console.log("req.url " + req.url);
 	console.log("req.method " + req.method);
 	console.log("url.pathname" + url.pathname);
 
-	if (req.method == "POST") {
-		postParser(req, res);
-	}
-
 	if (urlMatchesFormSubmit) {
-		console.log("found a match");
 		readAndResHtmlFile(req, res);
-		console.log(urlParsed);
-		console.log("url.pathname " + url.pathname);
+		console.log("found a match");
+		console.log(urlParsedObj);
 	} else {
 		readAndResHtmlFile(req, res);
 	}
-});
+}
 
 server.listen(port, host, () => {
 	console.log(`Listening: http://${host}:${port}`);
 });
-
+/*
 function postParser(req, res) {
 	//body data += lalala
 	console.log(req, "req");
 	console.log(res, "res");
 }
-
+*/
 function readAndResHtmlFile(req, res) {
 	readHtmlFile(indexHtml).then(data => {
 		var replacedData = JSON.stringify(commitsData, null, 2);
