438cb2eec9daa900c5b922634c614a076fd0284e
Coelacanth7 <galford151@gmail.com>
2017-09-15 20:38:23 -0700

    i got some url data somewhat parsed

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 const token = require("./apikey");
 const http = require("http");
 const fs = require("fs");
+const commitsData = require("./data/commits");
+const url = require("url");
 
 const indexHtml = "./public/index.html";
+const urlRegex = /\/commits\?/;
 
 var port = process.env.PORT || process.argv[2] || 3000;
 var host = "localhost";
 
 var server = http.createServer((req, res) => {
-	console.log("req made " + req.url);
+	var reqUrl = req.url;
+	var urlMatchesFormSubmit = urlRegex.test(req.url);
+	var urlParsed = url.parse(reqUrl, true).query;
+
+	console.log("urlMatchesFormSubmit " + urlMatchesFormSubmit);
+	console.log("req.url " + req.url);
+	console.log("req.method " + req.method);
+	console.log("url.pathname" + url.pathname);
+
 	if (req.method == "POST") {
 		postParser(req, res);
+	}
+
+	if (urlMatchesFormSubmit) {
+		console.log("found a match");
+		readAndResHtmlFile(req, res);
+		console.log(urlParsed);
+		console.log("url.pathname " + url.pathname);
 	} else {
 		readAndResHtmlFile(req, res);
 	}
@@ -28,8 +46,11 @@ function postParser(req, res) {
 
 function readAndResHtmlFile(req, res) {
 	readHtmlFile(indexHtml).then(data => {
+		var replacedData = JSON.stringify(commitsData, null, 2);
+		var dataReplacer = data;
+		dataReplacer = data.replace("{{ commitFeed }}", replacedData);
 		res.writeHead(200, { "content-Type": "text/html" });
-		res.end(data);
+		res.end(dataReplacer);
 	});
 }
 
