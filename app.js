879de2c93db97c8199dc8dc781d0cf37c0c37735
Aaron Saloff <aaronsaloff@gmail.com>
2017-09-24 10:59:16 -0400

    Scrubbing and Logging the Data - completed

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-const commitFile = require('./data/commits');
+var commitFile = require('./data/commits');
+const GithubApi = require('./lib/github_api_wrapper');
+require('dotenv').config();
+
+var github = GithubApi();
+github.authenticate(process.env.GITHUB_TOKEN);
 
 const port = 3000;
 const host = 'localhost';
@@ -18,10 +23,68 @@ var server = http.createServer((req, res) => {
       res.writeHead(200, {
         'Content-Type': 'text/html'
       });
-      var commitFeed = JSON.stringify(commitFile, null, 2);
-      data = data.replace('{{ commitFeed }}', commitFeed);
-      res.end(data);
-      console.log(params);
+
+      // if there are params
+      if (Object.keys(params).length !== 0) {
+        github.getCommits(params)
+
+        .then((commits) => {
+          // Scrub the commit data
+          commits = commits.map((obj) => {
+            return {
+              'message': obj.commit.message,
+              'author': obj.commit.author,
+              'url': obj.html_url,
+              'sha': obj.sha
+             };
+          });
+
+          // Add repo name to object for identification
+          var displayCommits = {};
+          displayCommits[params.repo] = commits;
+          commits = JSON.stringify(displayCommits, null, 2);
+
+          if (Object.keys(commitFile).length === 0) {
+            // Overwrite
+            fs.writeFileSync('./data/commits.json', commits);
+          } else {
+            // Get original file
+            var original = fs.readFileSync('./data/commits.json', 'utf8', 'r');
+
+            // Remove unnecessary curly braces
+            original = original.substr(0, original.length - 2);
+            commits = original + ',' + commits.substr(1, commits.length);
+
+            // Rewrite file
+            fs.writeFileSync('./data/commits.json', commits);
+          }
+
+          // reload the contents as JSON
+          commitFile = fs.readFileSync('./data/commits.json', 'utf8', 'r');
+
+          // Replace the feed
+          data = data.replace('{{ commitFeed }}', commitFile);
+
+          res.end(data);
+        })
+
+        .catch((err) => {
+          // Give red error message
+          data = data.replace('{{ commitFeed }}', `<p class="text-center" style="color:red;">${ JSON.parse(err.body).message }</p>`);
+          res.end(data);
+        });
+      } else { // No params
+        // If the commit file is empty
+        if (Object.keys(commitFile).length === 0) {
+          // Prompt user to fill out form
+          data = data.replace('{{ commitFeed }}', '<p class="text-center">Enter a Username and Repo in the form to view the commit feed.</p>');
+        } else {
+          // Place already saved data in the commit JSON file
+          var commitFeed = JSON.stringify(commitFile, null, 2);
+          data = data.replace('{{ commitFeed }}', commitFeed);
+        }
+        res.end(data);
+      }
     }
   });
 });
