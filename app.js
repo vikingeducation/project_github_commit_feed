649aa4c311004a7ef65cac2f113bfd71e52a70ac
Aaron Saloff <aaronsaloff@gmail.com>
2017-09-24 07:04:48 -0400

    Parsing Form Data - completed

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 const commitFile = require('./data/commits');
 
 const port = 3000;
 const host = 'localhost';
 
 var server = http.createServer((req, res) => {
+
+  var params = url.parse(req.url, true).query;
+
   fs.readFile('./public/index.html', 'utf8', (err, data) => {
     if (err) {
       res.writeHead(404);
@@ -17,6 +21,7 @@ var server = http.createServer((req, res) => {
       var commitFeed = JSON.stringify(commitFile, null, 2);
       data = data.replace('{{ commitFeed }}', commitFeed);
       res.end(data);
+      console.log(params);
     }
   });
 });
