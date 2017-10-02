89e01bcb194f65b794b9f1cf38931e0d47056871
maddiereddy <maddiereddy@gmail.com>
2017-10-02 14:02:43 -0700

    completed connecting form with Git API Wrapper

--- app.js
+++ app.js
@@ -9,21 +9,22 @@ const htmlFilePath = './public/index.html';
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// let github = wrapper.init();
+let github = wrapper.init();
+wrapper.authenticate(github);
 
-// wrapper.authenticate(github);
-// wrapper.getCommits(github, "maddiereddy", "project_what_have_you_done");
 
 const server = http.createServer((req, res) => {
   let request = url.parse(req.url, true);
+
   if (request.pathname !== '/stylesheets/style.css') {
-    console.log(`Username: ${request.query.user} \nRepo: ${request.query.repo} `);
+    wrapper.getCommits(github, request.query.user, request.query.repo);
   }
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    let data = fs.readFileSync(htmlFilePath);
-    data = data.toString().replace('{{ commitFeed }}', JSON.stringify(feed, null, '  '));
-    res.end(data);
+
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  let data = fs.readFileSync(htmlFilePath);
+  data = data.toString().replace('{{ commitFeed }}', JSON.stringify(feed, null, '  '));
+  res.end(data);
 });
 
 
