0b4aca600e3aaea21cf1c9be22f804648970b805
maddiereddy <maddiereddy@gmail.com>
2017-10-02 12:15:38 -0700

    completed initial server setup

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+
 const wrapper = require('./gitHubApiWrapper');
+let htmlFilePath = './public/index.html';
 
-let github = wrapper.init();
+const hostname = '127.0.0.1';
+const port = 3000;
 
-wrapper.authenticate(github);
-wrapper.getCommits(github, "maddiereddy", "project_what_have_you_done");
+// let github = wrapper.init();
+
+// wrapper.authenticate(github);
+// wrapper.getCommits(github, "maddiereddy", "project_what_have_you_done");
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(fs.readFileSync(htmlFilePath));
+});
+
+
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+
+
+
 
 
 // const GitHubApi = require("github");
