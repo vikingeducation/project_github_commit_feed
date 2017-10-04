e6e39d38ec76b2c47ea3c8a2b8a55cb4cc9d33b6
maddiereddy <maddiereddy@gmail.com>
2017-10-03 23:07:46 -0700

    html page not getting refreshed with new feed

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const url = require('url');
 const fs = require('fs');
 
 const wrapper = require('./gitHubApiWrapper');
-const feed = require('./data/commits.json');
 const htmlFilePath = './public/index.html';
 
 const hostname = '127.0.0.1';
@@ -12,28 +11,41 @@ const port = 3000;
 let github = wrapper.init();
 wrapper.authenticate(github);
 
+let feed = require('./data/commits.json');
+let data = fs.readFileSync(htmlFilePath);
 
 const server = http.createServer((req, res) => {
   let request = url.parse(req.url, true);
 
-  if (request.pathname !== '/stylesheets/style.css') {
-    wrapper.getCommits(github, request.query.user, request.query.repo);
-  }
+  // error check user and repo
+  let user = request.query.user;
+  let repo = request.query.repo;
+
+  if (checkValid(user) && checkValid(repo)) 
+    wrapper.getCommits(github, user.trim(), repo.trim());
+
+  console.log('hit me');
 
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  let data = fs.readFileSync(htmlFilePath);
-  data = data.toString().replace('{{ commitFeed }}', JSON.stringify(feed, null, '  '));
+  data = data.toString().replace('{{ commitFeed }}', JSON.stringify(feed, null, 2));
   res.end(data);
 });
-
-
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 
+function checkValid(str) {
+  if ((str !== undefined) && (str.trim().length > 0)) {
+    return true;
+
+  } else {
+    console.log(`${str} not valid input, try again`);
+    return false;
+  }
+}
 
 
 
--- gitHubApiWrapper.js
+++ gitHubApiWrapper.js
@@ -1,6 +1,7 @@
 const GitHubApi = require("github");
 const GITHUB_API_KEY = require('./config.json').token;
-
+const fs = require('fs');
+let feed = require('./data/commits');
 
 let wrapper = {};
 
@@ -22,8 +23,36 @@ wrapper.getCommits = (github, username, repo) => {
   	repo: repo
 	}, function(err, res) {
   	if (err) console.log(err);
-  	else console.log(JSON.stringify(res, null , "  "));
+  	else scrubData(res.data);
 	});
 };
+
+scrubData = (commits) => {
+
+  let results = commits.map((each) => {
+    let scrubbed = {}; 
+    scrubbed.message = each.commit.message;
+    scrubbed.author = each.commit.author;
+    scrubbed.url = each.html_url;
+    scrubbed.sha = each.sha;
+    return scrubbed;
+  });
+
+  if (feed.commits) {
+    results.forEach((element) => {
+      // check if record (by sha) already exists 
+      feed.commits.push(element);
+    });
+
+  } else {
+    feed.commits = results;
+
+  }
+
+  fs.writeFileSync('./data/commits.json', JSON.stringify(feed, null, 2), (err) => {
+    if (err) throw err;
+    console.log("saved commits appended successfully");
+  });
+}
 
 module.exports = wrapper;
