c3b6a8f5835783bc09b353a07c9841904df9f072
promilo <milind.shah@mail.utoronto.ca>
2017-10-09 13:02:38 -0400

    reconstructed server

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const host = "localhost";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end(fs.readFileSync('./public/index.html'))
-})
+  fs.readFile('./public/index.html', 'utf8', (err, data) => {
+      if (err) {
+        res.statusCode = 404;
+        res.end('404 not found');
+      }
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data);
+    });
+  });
 
 server.listen(port, host, () => {
   console.log("server running at localhost:3000")
