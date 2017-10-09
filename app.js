0e18f1ed9991ab3d4b65830c4e39f7b922e794f2
promilo <milind.shah@mail.utoronto.ca>
2017-10-09 14:08:01 -0400

    finished connecting github wrapper to the web application

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
+const github = require('./modules/github')
+github.authenticate()
+
 const host = "localhost";
 const port = 3000;
 
@@ -20,11 +23,15 @@ const server = http.createServer((req, res) => {
       });
 
       if (url.parse(req.url).pathname === '/commits'){
-        console.log("/commits is being called =>", url.parse(req.url))
-        console.log('/commits  params =>', url.parse(req.url, true).query)
         let formData = url.parse(req.url, true).query
-        console.log("params", formData);
-
+        github.getCommits(formData.username, formData.repo).then((commits)=>{
+          console.log(commits.data)
+        })
+        // console.log("params", formData);
+        // var gitmod = require('./index.js')(formData['username'], formData['repo'])
+        // console.log("github", gitmod)
+        // let data = github.getData
+        // console.log(data)
       }
       // console.log(url.parse(req.url, true))
       data = data.toString().replace( '{{ commitFeed }}', commits)
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const git = require("./modules/github");
 
 // User can insert their own api key here locally if they want to.
-git.authenticate();
-git.getCommits("promilo", "github-avatar-downloader").then(
-  commit => console.log(commit.data[0])
-)
+
+var gitModule = (user, repo) => {
+  var data
+  console.log("i am being called")
+  git.authenticate();
+  git.getCommits(user, repo).then(
+    // commit => console.log(commit.data[0])
+    commit => data = commit.data
+  )
+  var getData = () => {
+    return data
+  }
+}
+
+module.exports = gitModule
