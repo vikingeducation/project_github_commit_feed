78ae029d98a3c77b5f420a54583d0099316b3066
promilo <milind.shah@mail.utoronto.ca>
2017-10-09 13:44:12 -0400

    Was able to get form data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const url = require('url');
 
 const host = "localhost";
 const port = 3000;
@@ -8,6 +9,7 @@ const commitsJSON = require('./data/commits.json')
 var commits = JSON.stringify(commitsJSON, null, 2)
 
 const server = http.createServer((req, res) => {
+
   fs.readFile('./public/index.html', 'utf8', (err, data) => {
       if (err) {
         res.statusCode = 404;
@@ -17,6 +19,14 @@ const server = http.createServer((req, res) => {
         'Content-Type': 'text/html'
       });
 
+      if (url.parse(req.url).pathname === '/commits'){
+        console.log("/commits is being called =>", url.parse(req.url))
+        console.log('/commits  params =>', url.parse(req.url, true).query)
+        let formData = url.parse(req.url, true).query
+        console.log("params", formData);
+
+      }
+      // console.log(url.parse(req.url, true))
       data = data.toString().replace( '{{ commitFeed }}', commits)
       res.end(data);
     });
