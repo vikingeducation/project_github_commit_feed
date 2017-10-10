db8f66a4d10fa3f416078ad33ce8f08c73e64c83
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-10-10 07:27:56 +0200

    commit feed displays search results

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
-let commitFeed = require('./data/commits');
+const GithubApiWrapper = require('./lib/github');
 
 const port = process.env.PORT || 3000;
 const host = 'localhost';
+let commitFeed;
+
+const _saveCommits = (data, done) => {
+  const commitString = JSON.stringify(data, null, 2);
+  fs.writeFile('./data/commits.json', commitString, 'utf8', (err) => {
+    if (err) throw err;
+    done();
+  });
+};
 
 const server = http.createServer((req, res) => {
   const urlObj = url.parse(req.url, true);
-  console.log(urlObj.query.user);
-  console.log(urlObj.query.repo);
-  fs.readFile('./public/index.html', 'utf8', (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.end('404 Not Found');
-    } else {
-      res.writeHead(200, {
-        'Content-Type': 'text/html'
+  const { user, repo } = urlObj.query;
+
+  const github = new GithubApiWrapper();
+  const p = new Promise((resolve) => {
+    if (user) {
+      github.getCommits(user, repo, (results) => {
+        // console.dir(results, { depth: null, colors: true });
+        _saveCommits(results, resolve);
       });
-      commitFeed = JSON.stringify(commitFeed, null, 2);
-      res.end(data.replace('{{ commitFeed }}', commitFeed));
+    } else {
+      resolve();
     }
+  });
+  p.then(() => {
+    fs.readFile('./public/index.html', 'utf8', (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(200, {
+          'Content-Type': 'text/html'
+        });
+        commitFeed = JSON.parse(fs.readFileSync('./data/commits.json', 'utf8'));
+        commitFeed = JSON.stringify(commitFeed, null, 2);
+        res.end(data.replace('{{ commitFeed }}', commitFeed));
+      }
+    });
   });
 });
 
--- index.js
+++
@@ -1,7 +0,0 @@
-const GithubApiWrapper = require('./lib/github');
-
-const github = new GithubApiWrapper();
-
-github.getCommits('thomahau', 'assignment_royalty_free_music_player', (results) => {
-  console.dir(results, { depth: null, colors: true });
-});
