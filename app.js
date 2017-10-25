678f96c7ef7b4493730ae2c3ef3274f7cbe83cd6
DC <heyyodc-git@yahoo.com>
2017-10-25 00:09:54 -0700

    started webhooks

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const github = require('./lib/github-wrapper');
 
 const myJsonFile = './data/commits.json';
 const hostname = 'localhost';
-const port = 4000;
+const port = 3000;
+const _headers = {
+	"Content-Type": "text/html",
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Headers": "Content-Type",
+	"Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE"
+};
 
 github.authenticate(process.env.GITHUB_ACCESS_TOKEN);
 
@@ -23,23 +29,23 @@ const server = http.createServer( (req, res) => {
 				let params = getParams(queryString);
 				// console.log(`user = ${params[1]} and repo = ${params[2]}`);
 				let githubParams = {owner: params[1], repo: params[2] };
- 				let p1 = github.getRepoCommits(githubParams);
-				p1.then(result => {
+ 				github.getRepoCommits(githubParams)
+				.then(result => {
 					//console.log(res.data[0].commit); // traversing the object returned from github
 					let trimmedResult = github.trimMeDown(result.data);
 					let trimmedResultStringified = JSON.stringify(trimmedResult, null, " ");
-					let p2 = github.doWriting(myJsonFile, trimmedResultStringified);
-					return p2;
+					return github.doWriting(myJsonFile, trimmedResultStringified);
 				}).then(result => {
-					let p3 = github.reReadFile(myJsonFile);
-					return p3;
+					console.log(result);
+					return github.reReadFile(myJsonFile);
 				}).then(result => {
-					res.writeHead(200, {"Content-Type": "text/html"});
+					res.writeHead(200, _headers);
 					let goodToGo = readFileContents.replace('{{ commitFeed }}', result);
 					res.end(goodToGo);	
 				}).catch(catchError);
 			} else {
-		 		res.writeHead(200, {"Content-Type": "text/html"});
+				res.writeHead(200, _headers);
+		 		// res.writeHead(200, {"Content-Type": "text/html"});
 				let noContent = readFileContents.replace('{{ commitFeed }}', '');
 				res.end(noContent);					
 			}
