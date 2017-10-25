7f72786fdfe0e858436abaf17e02beca202d4d85
DC <heyyodc-git@yahoo.com>
2017-10-24 20:22:15 -0700

    continued work on 'scrubbing and logging data'. progress with promises

--- app.js
+++ app.js
@@ -24,18 +24,26 @@ const server = http.createServer( (req, res) => {
  				let p = github.getRepoCommits(githubParams);
 				p.then(result => {
 					//console.log(res.data[0].commit); // traversing the object returned from github
+					console.log('>>>>> in P1 THEN: ' + result);
 					let trimmedResult = github.trimMeDown(result.data);
 					let trimmedResultStringified = JSON.stringify(trimmedResult, null, " ");
 					//console.log(trimmedResStringified);
 					let path = './data/commits.json';
-					return github.doWriting(path, trimmedResultStringified);
-				}) 				
+					let p2 = github.doWriting(path, trimmedResultStringified);
+					p2.then(result => {
+						console.log('>>>>> in P2 THEN: ' + result);
+					})
+					.catch(err => {
+						console.log('>>>>> in P2 CATCH: ');
+						console.log(err);
+					});
+				})
 				.catch(err => {
+					console.log('>>>>> in P1 CATCH: ');
 					console.log(err); // verbose mode
 					// console.log('DC promise rejected: \n' + err); // one-sentence mode
 				});				
 			}
-
 	 		res.writeHead(200, {"Content-Type": "text/html"});
 			const myJsonFile = require('./data/commits.json');
 			const myStrFile = JSON.stringify(myJsonFile, null, 2);
