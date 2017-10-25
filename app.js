b4e8bef9ceeae2786e8388e48fd1c2f1595f45c4
DC <heyyodc-git@yahoo.com>
2017-10-25 16:02:52 -0700

    refactored code

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const fs = require('fs');
 const url = require('url');
 const github = require('./lib/github-wrapper');
 
-const myJsonFile = './data/commits.json';
 const hostname = 'localhost';
 const port = 3000;
+
+const contentFile = './data/commits.json';
+const noContent = '';
 const _headers = {
 	"Content-Type": "text/html",
 	"Access-Control-Allow-Origin": "*",
@@ -15,43 +17,115 @@ const _headers = {
 	"Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE"
 };
 
-github.authenticate(process.env.GITHUB_ACCESS_TOKEN);
+// const express = require('./lib/express');
+// const app = express();
 
-const server = http.createServer( (req, res) => {
+//
+//create routes here
+//
+// app.post('/github/webhooks', (req, res) => {
+// 	let data = req.body;	
+
+// 	if (req.headers['content-type'] === 'application/json') {
+// 		data = JSON.parse(req.body);
+// 		data = JSON.stringify(data, null, 2);
+// 	}
+// 	res.end(data);
+// });
+
+let render = (res, fileContents, replacement) => {
+	res.writeHead(200, _headers);
+	let content = fileContents.replace('{{ commitFeed }}', replacement);
+	res.end(content);	
+};
+
+const fileNotFound = (res) => {
+	res.writeHead(404);
+	res.end("404 Not Found");
+};
+
+//
+//handle all incoming HTTP requests here
+//
+let routerHandle = (req, res) => {
 	let htmlPath = __dirname + '/public/index.html';
-	fs.readFile(htmlPath, 'utf8', (err, readFileContents) => {
+	fs.readFile(htmlPath, 'utf8', (err, fileContents) => {
 		if (err) {
-			res.writeHead(404);
-			res.end("404 Not Found");
+			fileNotFound(res);
 		} else {
 			let queryString = url.parse(req.url).query;
 			if (queryString) {
-				let params = getParams(queryString);
-				// console.log(`user = ${params[1]} and repo = ${params[2]}`);
-				let githubParams = {owner: params[1], repo: params[2] };
- 				github.getRepoCommits(githubParams)
-				.then(result => {
-					//console.log(res.data[0].commit); // traversing the object returned from github
-					let trimmedResult = github.trimMeDown(result.data);
-					let trimmedResultStringified = JSON.stringify(trimmedResult, null, " ");
-					return github.doWriting(myJsonFile, trimmedResultStringified);
-				}).then(result => {
-					console.log(result);
-					return github.reReadFile(myJsonFile);
-				}).then(result => {
-					res.writeHead(200, _headers);
-					let goodToGo = readFileContents.replace('{{ commitFeed }}', result);
-					res.end(goodToGo);	
-				}).catch(catchError);
+				fetchRepoCommits(queryString, res, fileContents);
 			} else {
-				res.writeHead(200, _headers);
-		 		// res.writeHead(200, {"Content-Type": "text/html"});
-				let noContent = readFileContents.replace('{{ commitFeed }}', '');
-				res.end(noContent);					
+				render(res, fileContents, noContent);
 			}
 		}
 	});
-});
+};
+
+let fetchRepoCommits = (queryString, res, fileContents) => {
+	github.authenticate(process.env.GITHUB_ACCESS_TOKEN);
+
+	let params = getParams(queryString);
+	// console.log(`user = ${params[1]} and repo = ${params[2]}`);
+	let githubParams = {owner: params[1], repo: params[2] };
+	github.getRepoCommits(githubParams)
+	.then(result => {
+		//console.log(res.data[0].commit); // traversing the object returned from github
+		let scrubbedResult = scrub(result.data);
+		let content = JSON.stringify(scrubbedResult, null, 2);
+		return doWriting(contentFile, content);
+	}).then(result => {
+		console.log(result);
+		return reReadFile(contentFile);
+	}).then(result => {
+		render(res, fileContents, result);
+	}).catch(catchError);	
+};
+
+let doWriting = (path, fileContents) => {
+//	fs.appendFile(path, fileContents, 'utf8', (err) => {
+	return new Promise( (resolve, reject) => {
+		fs.writeFile(path, fileContents, 'utf8', (err) => {
+			if (err) reject(err);
+			resolve('Write / overwrite successful!');
+		});		
+	});
+};
+
+let reReadFile = (path) => {
+	return new Promise( (resolve, reject) => {
+		fs.readFile(path, 'utf8', (err, data) => {
+			if (err) reject(err);
+			resolve(data);
+		});
+	});
+};
+
+//takes an array and returns an object
+let _objectify = (myArray) => {
+	//keeping object structures consistent with Viking
+	let myObject = {};
+	myObject.foobar = myArray;	
+	let myFinalObject = {};
+	myFinalObject.Foobar = myObject;	
+	return myFinalObject;
+};
+
+let scrub = (result) => {
+	// console.log(result);
+	const newResult = result.map( (element) => {
+		const { commit: {author, message, url, sha} } = element; // my first destructuring
+		return {
+			author: author,
+			message: message,
+			url: url,
+			sha: sha
+		};
+	});
+	// console.log(newResult);
+	return _objectify(newResult);
+};
 
 let catchError = (err) => {
 	console.log(err);
@@ -62,6 +136,8 @@ let getParams = (url) => {
 	let params = regex.exec(url);
 	return params;
 };
+
+const server = http.createServer( routerHandle );
 
 server.listen(port, hostname, () => {
 	console.log(`Server running at http://${hostname}:${port}/`);
