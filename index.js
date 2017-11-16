9123b2109cd5a11809da63ea5b960b344bbe1da4
SamuelLangenfeld <samlangenfeld@gmail.com>
2017-11-16 12:00:24 -0500

    finished form

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var github = require("./lib/github");
 fs = require("fs");
 
-console.log(github.githubCommits('GeneTinderholm', 'project_github_commit_feed'));
-/*  .then(data => {
+var commits = github.githubCommits(
+  "GeneTinderholm",
+  "project_github_commit_feed"
+);
+commits
+  .then(data => {
     console.log(data);
   })
   .catch(err => {
     console.error(err);
   });
-*/
