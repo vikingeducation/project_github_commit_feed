4f0166fa7e2daf808fa14c3aaa3985212c057010
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-16 11:37:28 -0600

    tomatoes

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
 var github = require("./lib/github");
 fs = require("fs");
 var http = require('http');
+let url = require('url');
 
 var commits = github.githubCommits(
   "GeneTinderholm",
   "project_github_commit_feed"
 );
 
-var server = http.createServer((req, res){
-  
-});
+var server = http.createServer((req, res)=> {
+	path = url.parse(req.url).pathname;
+  if(path === '/'){
+  		displayRoot(req, res);
+  	}else if(path === '/commits'){
+  		displayCommits(req, res);
+  	}
+  });
+server.listen(3000, 'localhost');
+let displayRoot = (req, res) => {
+	fs.readFile('./public/index.html', 'utf8', (err, data) => {
+		if(err){
+			res.statusCode = 404;
+			res.end('404');
+		} else {
+			let body = '';
+			req.on('data', data => {
+				body += data;
+			})
+			req.on('end', () =>{
+				req.body = (body);
+				github.githubCommits(req.query)
+				  .then((err, data) => {
+				  	if(err){
+				  		throw err;
+				  	} else {
+				  		res.statusCode = 200;
+				  		let jData = JSON.stringify(data);
+				  		let body2 = body.replace('{{commitData}}', jData);
+				  		res.end(body2);
+				  	}
+				  })
+			})
+		}
+	});
+}
 
+var displayCommits = (req, res)=>{
+	fs.readFile('./public/index.html', 'utf8', (err, data) => {
+		if(err){
+			res.end('404');
+		} else {
+			req.quer
+			res.end(data);
+		}
+	});
+}
 commits
   .then(data => {
     console.log(data);
