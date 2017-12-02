1c58070f899205d33d1823c6981b3538278e1670
Jared Gebel <jaredjgebel@gmail.com>
2017-12-02 11:45:34 -0500

    Add promises

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ const githubAPI = require('./lib/wrapper');
 
 var path = './public/index.html';
 
-var commitsStr = JSON.stringify(commits, null, 2);
-
-var parseGet = (path) => {
+var parseGet = (path, callback) => {
    var str = url.parse(path);
    var re = /\=(\w+)/g;
    var matches = str.path.match(re);
@@ -16,24 +14,49 @@ var parseGet = (path) => {
    if(matches !== null) { 
       var user = matches[0].substring(1);
       var repo = matches[1].substring(1);
-      githubAPI.githubRepoCommits(user, repo);
    }
+   callback(user, repo);
 };
 
 var requestListener = (req, res) => {
+
+   // 2. Check for parameters and make github call
+   // 3. Return HTML
+  
    res.writeHead(200, {
       'Content-type':'text/html' 
    });
-   fs.readFile(path, 'utf8', (err, data) => {
-      if (err) { throw err; }
-      data = data.replace('{{ commitFeed }}', commitsStr);
-      res.write(data);
-      res.end();
-    });
-
-   parseGet(req.url);   
+   
+   parseGet(req.url, (user, repo) => {
+      // if both parameters exist
+      if((user) && (repo)) {
+         // in resolve of promise, call API
+         var APIresponse = new Promise((resolve, reject) => {
+            resolve(githubAPI.githubRepoCommits(user, repo)); 
+         }).then((value) => {
+            console.log(value);
+         });
+         
+         // APIresponse.then(() => {
+         //    // stringify results from JSON file
+         //    var commitsStr = JSON.stringify(commits, null, 2);
+         //    // only call fs.readFile if URL parameters are present and JSON parameters are correct
+         //    fs.readFile(path, 'utf8', (err, data) => {
+         //       if (err) { throw err; }
+         //       data = data.replace('{{ commitFeed }}', commitsStr);
+         //       res.write(data);
+         //       res.end();
+         //    });
+         // });
+         
+      } else {
+         // handle readFile without user and repo parameters
+         fs.readFile(path, 'utf8', (err, data) => {
+            if (err) { throw err; }
+            data = data.replace('{{ commitFeed }}', 'Select a user and repository.');
+         });
+      }
+   });   
 };
 
 http.createServer(requestListener).listen(3000);
-
-// 
