95a955e6c43b12e41b51ecb9dc734ddc77a71fec
Jared Gebel <jaredjgebel@gmail.com>
2017-12-03 13:47:52 -0500

    Add router

--- app.js
+++ app.js
@@ -3,8 +3,16 @@ const fs = require('fs');
 const url = require('url');
 // const commits = require('./data/commits.json');
 const githubRepoCommits = require('./lib/wrapper');
+var router = require('./lib/router');
 
 var path = './public/index.html';
+
+var _headers = {
+  "Content-Type": "text/html",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+  "Access-Control-Allow-Headers": "GET, POST, PUT, PATCH, DELETE"
+};
 
 var parseGet = (path, callback) => {
    var str = url.parse(path);
@@ -21,49 +29,7 @@ var parseGet = (path, callback) => {
 };
 
 var requestListener = (req, res) => {
-   // 1. Return HTML
-   // 2. Check for parameters and make github call
-   // 3. Return HTML
   
-   res.writeHead(200, {
-      'Content-type':'text/html' 
-   });
-
-  //  fs.readFile(path, 'utf8', (err, data) => {
-  //   if (err) { throw err; }
-  //   data = data.replace('{{ commitFeed }}', 'Select a user and repository.');
-  //   res.write(data);
-  //   res.end();
-  //   });
-   
-   parseGet(req.url, (user, repo) => {
-      // if both parameters exist
-      if((user) && (repo)) {
-         
-         var promise = githubRepoCommits.githubRepoCommits.getCommits(user, repo);
-         promise.then((output) => {
-            //  stringify results from JSON file
-             var commitsStr = JSON.stringify(output, null, 2);
-
-            //  only call fs.readFile if URL parameters are present and JSON parameters are correct
-            fs.readFile(path, 'utf8', (err, data) => {
-               if (err) { throw err; }
-               data = data.replace('{{ commitFeed }}', commitsStr);
-               res.write(data);
-               res.end();
-            });
-          });
-         
-      } else {
-         // handle readFile without user and repo parameters
-         fs.readFile(path, 'utf8', (err, data) => {
-            if (err) { throw err; }
-            data = data.replace('{{ commitFeed }}', 'Select a user and repository.');
-            res.write(data);
-            res.end();
-         });
-      }
-   });   
 };
 
-http.createServer(requestListener).listen(3000);
+http.createServer(router.handle).listen(3000);
