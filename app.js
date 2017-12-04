f988c231ebf3dcc8edc87f9d9bb0e0c9db0c0aa8
Jared Gebel <jaredjgebel@gmail.com>
2017-12-04 18:45:28 -0500

    Finished webhook routing

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const http = require('http');
 
-// const commits = require('./data/commits.json');
-const githubRepoCommits = require('./lib/wrapper');
 var router = require('./lib/router');
 
 http.createServer(router.handle).listen(3000);
